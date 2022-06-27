d74c53c0e5d08aadfd821dcb89ce343d908851a4
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-27 16:16:17 +0530

    fixed footer font size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 export default function Footer() {
 	return (
-		<div className="bg-white w-full flex justify-center">
+		<div className="bg-white w-full flex justify-center text-sm">
 			<div className="shadow-xl flex items-center px-2.5 mb-2.5 rounded-2xl">
 				<p className="text-indigo-500">
 					Join the agricultural community
 				</p>
-				<button className="text-white bg-gradient-to-b from-indigo-500 to-violet-500 rounded-full px-5 py-2.5 ml-2.5">
+				<button className="text-white font-bold bg-gradient-to-b from-indigo-500 to-violet-500 rounded-full px-5 py-2.5 ml-2.5">
 					DOWNLOAD GRAMODAYA
 				</button>
 			</div>
